b894f41c3ba98cc02ebd6bc3fca5b31a8aae687f
ofer <ofergavri@gmail.com>
2023-03-02 19:09:11 +0200

    added paging for filters container

--- src/app/cmps/stay-filter/stay-filter.component.ts
+++ src/app/cmps/stay-filter/stay-filter.component.ts
@@ -13,7 +13,7 @@ export class StayFilterComponent {
   faChevronLeft = faChevronLeft
 
   index = 0
-  itemsToShow = 5
+  itemsToShow = 3
   constructor(private breakpointObserver: BreakpointObserver) {
     this.breakpointObserver.observe([
       "(max-width:460px)"
@@ -26,37 +26,52 @@ export class StayFilterComponent {
       "(min-width:700px)"
     ]).subscribe((result: BreakpointState) => {
       if (result.matches) {
-        this.itemsToShow = 5
+        this.itemsToShow = 6
       }
     })
     this.breakpointObserver.observe([
       "(min-width:960px)"
     ]).subscribe((result: BreakpointState) => {
       if (result.matches) {
-        this.itemsToShow = 8
+        this.itemsToShow = 9
       }
     })
     this.breakpointObserver.observe([
       "(min-width:1400px)"
     ]).subscribe((result: BreakpointState) => {
       if (result.matches) {
-        this.itemsToShow = 9
+        this.itemsToShow = 15
       }
     })
   }
 
   onClickArrow(diff: number) {
-    this.index += diff * this.itemsToShow
+    this.index += diff * 3
   }
 
   checkRightArrow() {
-    return this.filters.length -1 >= this.index
+    return this.filters.length - 1 >= this.index + this.itemsToShow + 3
+  }
+
+  checkLeftArrow() {
+    return this.index - 3 > 0
   }
 
   getFilters() {
     return this.filters.slice(this.index, this.index + this.itemsToShow)
   }
+
   filters = [
+    { txt: 'OMG!', img: 'assets/img/filter-imgs/omg.jpg' },
+    { txt: 'Amazing views', img: 'assets/img/filter-imgs/amazing-views.jpg' },
+    { txt: 'Trending', img: 'assets/img/filter-imgs/trending.jpg' },
+    { txt: 'Golfing', img: 'assets/img/filter-imgs/golfing.jpg' },
+    { txt: 'Surfing', img: 'assets/img/filter-imgs/surfing.jpg' },
+    { txt: 'Mansions', img: 'assets/img/filter-imgs/mansions.jpg' },
+    { txt: 'Luxe', img: 'assets/img/filter-imgs/luxe.jpg' },
+    { txt: 'Private rooms', img: 'assets/img/filter-imgs/private-rooms.jpg' },
+    { txt: 'Lakefront', img: 'assets/img/filter-imgs/lake-front.jpg' },
+    { txt: 'Castles', img: 'assets/img/filter-imgs/castles.jpg' },
     { txt: 'OMG!', img: 'assets/img/filter-imgs/omg.jpg' },
     { txt: 'Amazing views', img: 'assets/img/filter-imgs/amazing-views.jpg' },
     { txt: 'Trending', img: 'assets/img/filter-imgs/trending.jpg' },
