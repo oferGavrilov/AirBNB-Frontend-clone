0af0703ebb45ca7868ca1deabbf2a4e31ff2fa6f
ofer <ofergavri@gmail.com>
2023-03-04 19:49:04 +0200

    added styles for home page

--- src/app/cmps/google-map/google-map.component.ts
+++ src/app/cmps/google-map/google-map.component.ts
@@ -10,17 +10,16 @@ export class GoogleMapComponent implements OnInit {
   constructor() { }
   ngOnInit() { }
   display: any
-  center:google.maps.LatLngLiteral = {lat:24, lng:12}
-  zoom = 4
+  center: google.maps.LatLngLiteral = { lat: 32.084, lng: 34.8 }
+  zoom = 11
 
-  
   moveMap(event: google.maps.MapMouseEvent) {
-    if(event.latLng != null)
-    this.center = (event.latLng.toJSON());
+    if (event.latLng != null)
+      this.center = (event.latLng.toJSON());
   }
-  
+
   move(event: google.maps.MapMouseEvent) {
-    if(event.latLng != null)
-    this.display = event.latLng.toJSON();
+    if (event.latLng != null)
+      this.display = event.latLng.toJSON();
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor() { }
+  amount: number = 1250
 
+  onChangeAmount(event: Event) {
+    this.amount = Number((event.target as HTMLInputElement).value)
+  }
+
+  getSumOfNights() {
+    return (this.amount / 193).toFixed(0)
+  }
 }
