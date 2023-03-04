beb1f03b2037d5bd034df2416eeb35eb0b8e5a76
ofer <ofergavri@gmail.com>
2023-03-04 21:39:58 +0200

    fix stays data

--- src/app/cmps/google-map/google-map.component.ts
+++ src/app/cmps/google-map/google-map.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Stay } from 'src/app/models/stay.model';
+import { StayService } from 'src/app/services/stay.service';
 
 @Component({
   selector: 'map',
@@ -7,19 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoogleMapComponent implements OnInit {
 
-  constructor() { }
-  ngOnInit() { }
+  constructor(private stayService: StayService) { }
+  ngOnInit() {
+
+  }
+
   display: any
   center: google.maps.LatLngLiteral = { lat: 32.084, lng: 34.8 }
   zoom = 11
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  // getPositions() {
+  //   this.stays$ = this.stays$.map(pos => pos.loc)
+  // }
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) {
+      this.markerPositions.push(event.latLng.toJSON());
+    }
+  }
 
   moveMap(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null)
+    if (event.latLng != null) {
       this.center = (event.latLng.toJSON());
+    }
   }
 
   move(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null)
+    if (event.latLng != null) {
       this.display = event.latLng.toJSON();
+    }
   }
 }
